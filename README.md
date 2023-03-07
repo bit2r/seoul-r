5a537d707d916629bd6520f5772fdaf394c283b4
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2023-03-07 13:57:52 +0900

    initial draft for new seoul R meetup website

---
+++ README.md
@@ -0,0 +1,6 @@
+# seoul-R
+
+서울R미트업 (Seoul R Meetup)
+
+- Quaro로 관리되며 생성됩니다.
+- r2bit.com 하위에 위치하며 실제 배포되는 사이트는 r2bit.com/seoul-r 입니다.
---
+++ requirements.R
@@ -0,0 +1,7 @@
+# 웹사이트를 빌드하는데 필요한 패키지들
+
+# install.packages("lubridate")
+# install.packages("openxlsx")
+# install.packages("showtext")
+# install.packages("cowplot")
+# install.packages("janitor")
